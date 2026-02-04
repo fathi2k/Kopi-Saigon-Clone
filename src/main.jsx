3586abba76968c1850505a969c4fb625f79cfaa9
Fathi <fathisyameer22@gmail.com>
2026-02-04 20:33:17 +0800

    -adjust router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import { RouterProvider,createBrowserRouter } from 'react-router-dom'
 import Menu from './atomic/page/Menu.jsx'
 import HomePage from './atomic/page/HomePage.jsx'
